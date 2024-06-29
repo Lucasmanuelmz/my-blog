ae227faa8e2f13d194f9971ecb73c64465d20ce1
Lucas Manueel <testadormoz@gmail.com>
2024-06-29 21:50:28 +0200

    Adicionei classes para estilizar esta página

--- routes/categories/index.js
+++ routes/categories/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/',(req, res) => {
-  res.render('index');
+router.get('/dashboard/category',(req, res) => {
+  res.render('dashboard/categories/index.ejs');
 });
 
 module.exports = router;
