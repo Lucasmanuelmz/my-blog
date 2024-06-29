17d08a56e9abdf3a54355ec5a50e5f577aa386c1
Lucas Manueel <testadormoz@gmail.com>
2024-06-29 22:54:40 +0200

    Adicionei duas rotas neste arquivo: Rota de tabela para exibir dados e rota de cadastro de categorias

--- routes/categories/index.js
+++ routes/categories/index.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const Categories = require('./Categorie');
 const router = express.Router();
+const slugify = require('slugify')
 
 router.get('/dashboard/category',(req, res) => {
   res.render('dashboard/categories/index.ejs');
 });
+
+router.post('/add-categories', (req, res) => {
+  let {title} = req.body;
+  Categories.create({
+    title: title,
+    slug: slugify(title)
+  }).then(() => {
+    res.redirect('dasboard/categories/new')
+  })
+})
+
+router.get('/dasboard/categories/new', (req, res) => {
+  Categories.findAll().then((categories) => {
+    if(categories != undefined) {
+      res.render('dashboard/categories/newcat.ejs',{
+        categories: categories
+      })
+    }else {
+      console.log('Categoria não encontrada')
+    }
+  })
+})
 
 module.exports = router;
